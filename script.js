fe4b1dbffeeafd87b0c99e462f47dad93336cda5
13010-34-08 <13010-34-08@exchange.ad.afpanet>
2023-10-10 16:54:00 +0200

    début d'exercice

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Hello World");
+
+let listeMots = ["Cachalot", "Pétunia", "Serviette"];
+let score = 0;
+let motUtilisateur = prompt("Ecrire ici:" + listeMots[0])
+    if (motUtilisateur == listeMots[0]) {
+        console.log("Bravo !")
+        score += 1
+        console.log(score)
+    } else {
+        console.log("Mince, vous vous êtes trompé !")
+        console.log(score)
+    }
+
+let motUtilisateur2 = prompt("Ecrire ici:" + listeMots[1])
+    if (motUtilisateur == listeMots[1]) {
+    console.log("Bravo !")
+    score += 1
+    console.log(score)
+    } else {
+    console.log("Mince, vous vous êtes trompé !")
+    console.log(score)
+    }
+
+let motUtilisateur3 = prompt("Ecrire ici:" + listeMots[2])
+    if (motUtilisateur == listeMots[2]) {
+    console.log("Bravo !")
+    score += 1
+    console.log(score)
+    } else {
+    console.log("Mince, vous vous êtes trompé !")
+    console.log(score)
+    }
