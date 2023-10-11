6bbc98b363f3d959b1b2307ecb6044a1c0f56db0
13010-34-08 <13010-34-08@exchange.ad.afpanet>
2023-10-11 16:02:07 +0200

    ajout fonctions

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 console.log("Hello World");
 
-function afficherResultat (score, totalMots) {
-    let message : "Votre score est de " + score + "sur " + totalMots
-return message}
-
-function ChoisirPhrasesOuMots {
-    let message : "Veuillez choisir la liste: mots ou phrases"
-    return message
-}
-
 let listeMots = ["Cachalot", "Pétunia", "Serviette"];
 let listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"];
-let score = 0;
+listePropositions = listeMots + listePhrases
+totalMots = listeMots.lenght || listePhrases.length
+let score = 0
 
-let choix= prompt("Veuillez choisir la liste: mots ou phrases")
+function choisirPhrasesouMots () {
+    let choix= prompt("Veuillez choisir la liste: mots ou phrases")
 while (choix !== "mots" && choix !== "phrases") {
     choix= prompt("Veuillez choisir la liste: mots ou phrases")}
-    if (choix === "mots"){
-        for (let i = 0; i < listeMots.length; i ++){
-            let motUtilisateur = prompt("Ecrire ici: " + listeMots[i]);
-            if (motUtilisateur === listeMots[i]) {
-                console.log("Bravo !")
-                score += 1 }
-             } console.log("Votre score est de " + score + " sur " + listeMots.length)
-    } else {
-    for (let i = 0; i < listePhrases.length; i ++){
-        let motUtilisateur = prompt("Ecrire ici: " + listePhrases[i]);
-        if (motUtilisateur === listePhrases[i]) {
+}
+
+function lancerBoucleDeJeu (listePropositions){
+    for (let i = 0; i < listePropositions.length; i ++){
+        let motUtilisateur = prompt("Ecrire ici: " + listeMots[i]);
+        if (motUtilisateur === listeMots[i]) {
             console.log("Bravo !")
             score += 1 }
-             }      console.log("Votre score est de " + score + " sur " + listeMots.length) 
+         } 
+         return score }
+
+    function lancerJeu() {
+        choix=choisirPhrasesouMots
+        let score = 0;
+        let listePropositions = 0
+
+    function afficherResultat (score, totalMots) {
+    console.log("Votre score est de " + score + " sur " + totalMots)}
+
+        if (choix === "mots"){
+            lancerBoucleDeJeu(listeMots)
+            listePropositions = listeMots.length
+        } else {
+            lancerBoucleDeJeu(listePhrases)
+            listePropositions = listePhrases.lenght
+        }
+        afficherResultat(score, totalMots)
     }
+    
+
+    lancerJeu()
